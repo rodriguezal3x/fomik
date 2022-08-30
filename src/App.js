b1e4b29caf86e3e6ffdaad758ba66c3ba1b436e7
rodriguezal3x <alexjosuerm1995@gmail.com>
2022-08-30 10:08:20 -0600

    FORMIK CON COMPONENT

--- src/App.js
+++ src/App.js
@@ -1,9 +1,51 @@
-import React from 'react'
+import { Formik } from "formik";
 
-const App = () => {
-  return (
-    <div>App</div>
-  )
+const validate = ( values ) => {
+  const errors= {}
+
+  if (!values.name) {
+    errors.name = 'Requerido'        
+  } else if (values.name.length < 5) {
+    errors.name = 'El nombre es muy corto'
+  }
+
+  if (!values.lastname) {
+    errors.lastname = 'Requerido'        
+  } else if (values.lastname.length < 5) {
+    errors.lastname = 'El apellido es muy corto'
+  }
+
+  return errors
 }
 
-export default App
+function App() {
+  return (
+    <Formik
+        initialValues={{name:'', lastname:'', email:'', }}
+        validate={ validate }
+        onSubmit={ values => console.log(values) } 
+    >
+        { formik =>
+            <form onSubmit={formik.handleSubmit}>
+                <label>Nombre</label>
+                <input type='text' {...formik.getFieldProps('name')}/>
+                {formik.touched.name && formik.errors.name ? 
+                    <div>{formik.errors.name}</div> : null }
+                <br/>
+                <label>Apellido</label>
+                <input type='text'{...formik.getFieldProps('lastname')}/>
+                { formik.touched.lastname && formik.errors.lastname ? 
+                    <div>{formik.errors.lastname}</div> : null }
+                <br/>
+                <label>Correo</label>
+                <input type='email' {...formik.getFieldProps('email')}/>
+                { formik.touched.email && formik.errors.email ? 
+                  <div>{formik.errors.email}</div> : null }
+                <button type="submit">Enviar</button>
+            </form>
+        }
+    </Formik>
+  );
+}
+
+export default App;
