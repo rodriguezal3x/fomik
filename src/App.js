892f1071e8513f1baa77402ca4f7eeafa0877445
rodriguezal3x <alexjosuerm1995@gmail.com>
2022-08-30 22:09:25 -0600

    formulario finalizado y optimizado con formik

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import { Formik, Form,} from "formik";
+import { Formik, Form, Field, ErrorMessage } from "formik";
 import TextInput from "./components/TextInput";
 import Checkbox from "./components/Checkbox";
+import Radio from "./components/Radio";
+import Select from "./components/Select";
 
 const validate = ( values ) => {
   const errors= {}
@@ -17,13 +19,17 @@ const validate = ( values ) => {
     errors.lastname = 'El apellido es muy corto'
   }
 
+  if (!values.radio) {
+    errors.radio ='Requerido'
+  }
+
   return errors
 }
 
 function App() {
   return (
     <Formik
-        initialValues={{name:'', lastname:'', email:'', }}
+        initialValues={{ name:'', lastname:'', email:'', genero:'', radio:'' }}
         validate={ validate }
         onSubmit={ values => console.log(values) } 
     >
@@ -33,9 +39,19 @@ function App() {
             <TextInput name='lastname' label='Apellido'/>
             <br/>
             <TextInput name='email' label='Correo'/>
+            <Select label='Genero' name='genero'>
+                <option value=''>Selecione uno</option>
+                <option value='mujer'>Mujer</option>
+                <option value='hombre'>Hombre</option>
+                <option value='otro'>Otro</option>
+            </Select>
             <Checkbox name="accep">
                 Aceptar terminos y Condiciones
             </Checkbox>
+            <Radio  name='radio' value='chanchito1' label='Chanchito 1' />
+            <Radio  name='radio' value='chanchito2' label='Chanchito 2' />
+            <Radio  name='radio' value='chanchito3' label='Chanchito 3' />
+            <ErrorMessage name="radio"/>
             <button type="submit">Enviar</button>
         </Form>
     </Formik>
